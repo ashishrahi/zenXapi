597ff92015efc6edba9697ca3ac7bc881ffb90ea
ashishrahi <148324855+ashishrahi@users.noreply.github.com>
2025-10-01 22:04:01 +0530

    change order api

--- src/api/v1/repository/orderRepository.ts
+++ src/api/v1/repository/orderRepository.ts
@@ -11,7 +11,7 @@ export const orderRepository = {
 
   // Find All Orders
 findAllOrders: async (filter?: Partial<IOrders>) => {
-  return await Orders.find(filter || {}).populate("userId", "name email").populate("productId","name")
+  return await Orders.find(filter || {}).populate("userId", "name email").populate("products.product","name")
 },
 
   // Find Order by ID
